af21b15f5a7c41a8a475dcff11762046ff7258f2
voxmenthe <jeffcogg@gmail.com>
2019-10-06 19:50:36 -0700

    automated commit from home

--- BASKETS/make_baskets.py
+++ BASKETS/make_baskets.py
@@ -1,9 +1,15 @@
 import gc
 import time
 import pickle
-from get_baskets import get_baskets
 
-DATA_PATH = '/home/jovyan/data/yh09262/ur_train_20190131_46/'
+sys.path.append('/home/jovyan/work/a091569/cython_home')
+sys.path.append('/home/jovyan/work/a091569/utils_and_queries')
+
+from get_baskets import get_baskets
+from get_baskets_cdef import get_baskets_cdef
+
+DATA_PATH = '/home/jovyan/datasets/d_train_20190923_60_0/'
+#DATA_PATH = '/home/jovyan/data/yh09262/ur_train_20190131_46/'
 #DATA_PATH = '/home/jovyan/data/yh09262/ur_train_20190220_3/'
 
 timestamp = time.ctime()
@@ -12,6 +18,8 @@ timestamp = timestamp.replace(" ","_").replace(":","")
 combined_dict = {}
 
 viewdict = get_baskets(DATA_PATH + 'ur_production_view.csv',min_n=1)
+with open('viewdict__20190923_60.pkl', 'wb') as f:
+    pickle.dump(viewdict, f)
 
 for key, value in viewdict.items():
     if key not in combined_dict:
@@ -22,6 +30,8 @@ del viewdict
 gc.collect()
 
 atbdict = get_baskets(DATA_PATH + 'ur_production_atb.csv',min_n=1)
+with open('atbdict__20190923_60.pkl', 'wb') as f:
+    pickle.dump(atbdict, f)
 
 for key, value in atbdict.items():
     if key not in combined_dict:
@@ -32,6 +42,8 @@ del atbdict
 gc.collect()
 
 purdict = get_baskets(DATA_PATH + 'ur_production_purchase.csv',min_n=1)
+with open('purdict__20190923_60.pkl', 'wb') as f:
+    pickle.dump(purdict, f)
 
 for key, value in purdict.items():
     if key not in combined_dict:
@@ -41,7 +53,8 @@ for key, value in purdict.items():
 del purdict
 gc.collect()
 
-with open('comb_dict_20190131_46_{}_m1.pkl'.format(timestamp), 'wb') as f:
+with open('comb_dict_20190923_60_{}.pkl'.format(timestamp), 'wb') as f:
     pickle.dump(combined_dict, f)
 
-print("Saved combined_dict as comb_dict_{}_m1.pkl".format(timestamp))
+print("Saved combined_dict as comb_dict_20190923_60_{}.pkl".format(timestamp))
+
