4f020385c5bc3b229f4d6c3387130dbcf9a88c22
Jeff Coggshall <jeff.coggshall@macys.com>
2019-10-18 16:55:30 -0700

    automated commit from laptop Fri Oct 18 16:55:25 PDT 2019

--- hive_queries/etl_tables.sh
+++ hive_queries/etl_tables.sh
@@ -2,6 +2,7 @@
 
 LIM=30000
 
+# 107
 hive -e"select * FROM ltrfc.cm_cart_item_addition WHERE dt > $1 LIMIT $2" >> cm_cart_item_addition_$1_$2.csv
 hive -e"select * FROM ltrfc.cm_cart_item_purchase WHERE dt > $1 LIMIT $2" >> cm_cart_item_purchase_$1_$2.csv
 hive -e"select * FROM ltrfc.cm_order WHERE dt > $1 LIMIT $2" >> cm_order_$1_$2.csv
